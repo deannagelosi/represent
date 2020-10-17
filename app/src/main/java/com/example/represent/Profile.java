65ccb96898ee8e3e142ed632bb3950b5ade614e3
Deanna Gelosi <deannalosi@gmail.com>
2020-10-16 22:18:43 -0700

    bio loads on profile

--- app/src/main/java/com/example/represent/Profile.java
+++ app/src/main/java/com/example/represent/Profile.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Profile extends AppCompatActivity {
 
     // Declare widgets
     ImageButton back;
+    TextView bio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,7 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         back = findViewById(R.id.back);
+        bio = findViewById(R.id.bio);
 
         final String officialsString = getIntent().getExtras().getString("officials");
         final Integer index = getIntent().getExtras().getInt("index");
@@ -31,5 +42,24 @@ public class Profile extends AppCompatActivity {
             }
         });
 
+        String repBio = "";
+
+        try {
+            JSONArray officials = new JSONArray(officialsString);
+            Log.d( "official profile", "" + officials.getJSONObject(index));
+            JSONObject official = officials.getJSONObject(index);
+
+            String office = official.getString("office");
+            String name = official.getString("name");
+            String party = official.getString("party");
+            String imageUrl = official.optString("photoUrl");
+
+            repBio = "Your " + office + " is " + name + " who is a member of the " + party + " party.";
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        bio.setText(repBio);
     }
 }
